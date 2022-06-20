add1a1eb7aaecb64acb115190325432ae0b3d232
Muhammad Ibrahim <ibrahimraju347@gmail.com>
2022-06-20 15:26:23 +0500

    Get the pages form werdpress

--- pages/ProfileUpdate.js
+++ pages/ProfileUpdate.js
@@ -1,84 +1,55 @@
 import React from "react";
 
-function ProfileUpdate() {
+function ProfileUpdate({ data }) {
+  console.log("Data is", data);
   return (
-    <div className='grid lg:grid-cols-3 gap-3 justify-items-center p-5 md:grid-cols-2 sm:grid-cols-1'>
+    <div className='grid lg:grid-cols-1  justify-items-center p-5 md:grid-cols-2 '>
       {/* ................................Content 1........................ */}
+      {data.map((page, index) => {
+        return (
+          <div>
+            <div>
+              {page["_embedded"]["wp:featuredmedia"] && (
+                <img
+                  className='lg:h-48 md:h-full w-full object-cover object-center'
+                  src={page["_embedded"]["wp:featuredmedia"][0]["source_url"]}
+                  alt='blog'
+                />
+              )}
+            </div>
 
-      <div>
-        <div>
-          <img
-            className='lg:h-48 md:h-full w-full object-cover object-center'
-            src='https://dummyimage.com/720x400'
-            alt='blog'
-          />
-        </div>
-
-        <div className='p-6'>
-          <h1 className='title-font text-lg font-medium text-gray-900 mb-3'>
-            The Catalyzer
-          </h1>
-          <p className='leading-relaxed mb-3'>
-            Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-            microdosing tousled waistcoat.
-          </p>
-          <a className='text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0'>
-            {" "}
-            More
-          </a>
-        </div>
-      </div>
-      {/* ................................Content 2........................ */}
-      <div>
-        <div>
-          <img
-            className='lg:h-48 md:h-full w-full object-cover object-center'
-            src='https://dummyimage.com/720x400'
-            alt='blog'
-          />
-        </div>
-
-        <div className='p-6'>
-          <h1 className='title-font text-lg font-medium text-gray-900 mb-3'>
-            The Catalyzer
-          </h1>
-          <p className='leading-relaxed mb-3'>
-            Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-            microdosing tousled waistcoat.
-          </p>
-          <a className='text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0'>
-            {" "}
-            More
-          </a>
-        </div>
-      </div>
-
-      {/* ................................Content 3........................ */}
-      <div>
-        <div>
-          <img
-            className='lg:h-48 md:h-full w-full object-cover object-center'
-            src='https://dummyimage.com/720x400'
-            alt='blog'
-          />
-        </div>
-
-        <div className='p-6'>
-          <h1 className='title-font text-lg font-medium text-gray-900 mb-3'>
-            The Catalyzer
-          </h1>
-          <p className='leading-relaxed mb-3'>
-            Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-            microdosing tousled waistcoat.
-          </p>
-          <a className='text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0'>
-            {" "}
-            More
-          </a>
-        </div>
-      </div>
+            <div className='p-6'>
+              <h1 className='title-font text-lg font-medium text-gray-900 mb-3'>
+                {page.title.rendered}
+              </h1>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: page["content"]["rendered"],
+                }}
+                className='leading-relaxed text-base'
+              ></div>
+              <a className='text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0'>
+                {" "}
+                More
+              </a>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const getPosts = await fetch(
+    "http://blogs.local//wp-json/wp/v2/pages?_embed&&slug=profile-update"
+  );
+  const data = await getPosts.json();
+  // const d3 = data.map((d) => {
+  //   return d["_embedded"]["wp:featuredmedia"][0]["source_url"];
+  // });
+
+  return { props: { data } };
 }
 
 export default ProfileUpdate;
